6ea4562859eb8291c621371ab441535b4801ebc3
Carlossuarez010 <chaparralsuarezcarlosandres@gmail.com>
2024-11-22 21:25:37 -0500

    Signed-off-by: Carlossuarez010 <chaparralsuarezcarlosandres@gmail.com>

---
+++ Objpila.java
@@ -0,0 +1,20 @@
+
+import java.util.Stack;
+
+public class Objpila {
+    Stack<Integer>numeros;
+    Stack<Integer>Ppilas;
+    public Stack<Integer> getNumeros() {
+        return numeros;
+    }
+    public void setNumeros(Stack<Integer> numeros) {
+        this.numeros = numeros;
+    }
+    public Stack<Integer> getPpilas() {
+        return Ppilas;
+    }
+    public void setPpilas(Stack<Integer> ppilas) {
+        Ppilas = ppilas;
+    }
+   
+}
